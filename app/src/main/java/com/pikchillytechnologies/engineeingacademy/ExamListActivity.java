45f9840e7daa490bff98d66068f3cea8e2694899
arunlakhera@gmail.com <arunlakhera@gmail.com>
2019-01-29 21:43:35 +0530

    All Screens added

---
+++ app/src/main/java/com/pikchillytechnologies/engineeingacademy/ExamListActivity.java
@@ -0,0 +1,13 @@
+package com.pikchillytechnologies.engineeingacademy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_list);
+    }
+}
