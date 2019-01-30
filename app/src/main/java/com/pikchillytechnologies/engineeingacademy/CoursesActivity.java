540dc583c8917162134251f98628cfc63089b016
arunlakhera@gmail.com <arunlakhera@gmail.com>
2019-01-30 14:44:52 +0530

    All Screens added

--- app/src/main/java/com/pikchillytechnologies/engineeingacademy/CoursesActivity.java
+++ app/src/main/java/com/pikchillytechnologies/engineeingacademy/CoursesActivity.java
@@ -1,13 +1,28 @@
 package com.pikchillytechnologies.engineeingacademy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class CoursesActivity extends AppCompatActivity {
+
+    CardView m_CardView_Mechanical;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        m_CardView_Mechanical = findViewById(R.id.cardView_Mechanical);
+
+        m_CardView_Mechanical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CoursesActivity.this, SubCoursesActivity.class));
+            }
+        });
+
     }
 }
