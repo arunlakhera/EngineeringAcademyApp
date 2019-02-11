05627d3f76828b73a3598dfbdf76b4afe0d4962e
arunlakhera@gmail.com <arunlakhera@gmail.com>
2019-02-11 21:26:21 +0530

    All Screens added

--- app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/SignInActivity.java
+++ app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/SignInActivity.java
@@ -1,20 +1,43 @@
 package com.pikchillytechnologies.engineeingacademy.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.pikchillytechnologies.engineeingacademy.HelperFiles.EAHelper;
 import com.pikchillytechnologies.engineeingacademy.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText m_Email_Id_TextView;
+    private EditText m_Password_TextView;
 
     private Button m_Sign_In_Button;
     private TextView m_Sign_Up_TextView;
     private EAHelper m_Helper;
+
+    private ProgressDialog pd;
+
+    private static String loginURL ="https://pikchilly.com/api/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +46,11 @@ public class SignInActivity extends AppCompatActivity {
 
         m_Helper = new EAHelper();
 
+        pd = new ProgressDialog(SignInActivity.this);
+
+        m_Email_Id_TextView = findViewById(R.id.edittext_Email_Id);
+        m_Password_TextView = findViewById(R.id.edittext_Password);
+
         m_Sign_Up_TextView = findViewById(R.id.textview_New_Sign_Up);
         m_Sign_In_Button = findViewById(R.id.button_Sign_In);
 
@@ -30,7 +58,10 @@ public class SignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                m_Helper.start_Activity(SignInActivity.this, CoursesActivity.class);
+                //m_Helper.start_Activity(SignInActivity.this, CoursesActivity.class);
+
+                signInRequest();
+
             }
         });
 
@@ -42,4 +73,62 @@ public class SignInActivity extends AppCompatActivity {
         });
 
     }
+
+    public void signInRequest(){
+
+        pd.setMessage("Signing In . . .");
+        pd.show();
+
+        RequestQueue m_Queue = Volley.newRequestQueue(SignInActivity.this);
+        String response = null;
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, loginURL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+
+                       pd.hide();
+
+                        if(response.equals("Sign_In_Success")) {
+
+                            m_Helper.start_Activity(SignInActivity.this, CoursesActivity.class);
+
+                        }else if(response.equals("Sign_In_Failed")){
+                            Toast.makeText(getApplicationContext(),"User Name/Password does not match. Try Again.",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Error Occured. Please Try Again.",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        pd.hide();
+
+                        String err = (error.getMessage()==null)?"Error is:":error.getMessage();
+
+                        Log.d("ErrorResponse", err);
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("username", m_Email_Id_TextView.getText().toString());
+                params.put("password", m_Password_TextView.getText().toString());
+                return params;
+            }
+        };
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        m_Queue.add(postRequest);
+
+    }
+
 }
--- app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/SignUpActivity.java
+++ app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/SignUpActivity.java
@@ -1,23 +1,57 @@
 package com.pikchillytechnologies.engineeingacademy.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.pikchillytechnologies.engineeingacademy.HelperFiles.EAHelper;
 import com.pikchillytechnologies.engineeingacademy.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText m_First_Name_EditText;
+    private EditText m_Last_Name_EditText;
+    private EditText m_Phone_EditText;
+    private EditText m_Email_Id_EditText;
+    private EditText m_Password_EditText;
+
     private Button m_Sign_Up_Button;
     private TextView m_Sign_In_TextView;
+    private ProgressDialog pd;
+    private EAHelper m_Helper;
+
+    private static String signupURL ="https://pikchilly.com/api/signup.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        m_Helper = new EAHelper();
+        pd = new ProgressDialog(SignUpActivity.this);
+
+        m_First_Name_EditText = findViewById(R.id.edittext_First_Name);
+        m_Last_Name_EditText = findViewById(R.id.edittext_Last_Name);
+        m_Phone_EditText = findViewById(R.id.edittext_Phone);
+        m_Email_Id_EditText = findViewById(R.id.edittext_Email_Id);
+        m_Password_EditText = findViewById(R.id.edittext_Password);
 
         m_Sign_Up_Button = findViewById(R.id.button_Sign_Up);
         m_Sign_In_TextView = findViewById(R.id.textview_Sign_In);
@@ -25,16 +59,75 @@ public class SignUpActivity extends AppCompatActivity {
         m_Sign_Up_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignUpActivity.this, CoursesActivity.class));
+                signupRequest();
             }
         });
 
         m_Sign_In_TextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+                m_Helper.start_Activity(SignUpActivity.this, SignInActivity.class);
             }
         });
 
     }
+
+    public void signupRequest(){
+
+        pd.setMessage("Signing Up . . .");
+        pd.show();
+
+        RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
+        String response = null;
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, signupURL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+
+                        pd.hide();
+
+                        if(response.equals("SignUp_Success")) {
+
+                            m_Helper.start_Activity(getApplicationContext(), CoursesActivity.class);
+
+                        }else if(response.equals("SignUp_Failed")){
+
+                            Toast.makeText(getApplicationContext(),"Could not Sign Up",Toast.LENGTH_LONG).show();
+                        }else if(response.equals("UsernameExists")){
+                            Toast.makeText(getApplicationContext(),"User with this email already exists!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("ErrorResponse", error.getMessage());
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+
+                params.put("firstname", m_First_Name_EditText.getText().toString());
+                params.put("lastname", m_Last_Name_EditText.getText().toString());
+                params.put("phonenumber", m_Phone_EditText.getText().toString());
+                params.put("username", m_Email_Id_EditText.getText().toString());
+                params.put("password", m_Password_EditText.getText().toString());
+
+                return params;
+            }
+        };
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(postRequest);
+
+    }
+
+
 }
