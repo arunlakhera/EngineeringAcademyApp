7b1b54b79e2be7ee902ce9a7eeb39e3ceb92c690
arunlakhera@gmail.com <arunlakhera@gmail.com>
2019-03-08 13:46:26 +0530

    My Downloads Completed

--- app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/MyDownloadsActivity.java
+++ app/src/main/java/com/pikchillytechnologies/engineeingacademy/Activity/MyDownloadsActivity.java
@@ -5,11 +5,15 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -41,6 +45,12 @@ public class MyDownloadsActivity extends AppCompatActivity {
     private MyDownloadsAdapter m_DownloadFile_Adapter;
 
     private String fileName;
+    private RecyclerView.LayoutManager m_Layout_Manager;
+
+    //Navigation Drawer
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
+    private Button menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,17 +61,18 @@ public class MyDownloadsActivity extends AppCompatActivity {
         m_User_Id = m_User_Bundle.getString(getResources().getString(R.string.userid), "User Id");
         m_User_Name = m_User_Bundle.getString("username", "User Name");
 
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         m_TextView_Activity_Title = findViewById(R.id.textView_Activity_Title);
-        m_TextView_Activity_Title.setText("My Downloads");
-
         m_Button_Back = findViewById(R.id.button_Back);
+        m_RecyclerView_DownloadedFile = findViewById(R.id.recyclerView_MyDownloads);
+        navigationView = findViewById(R.id.nav_view);
+        menuButton = findViewById(R.id.button_Menu);
+
+        m_TextView_Activity_Title.setText("My Downloads");
         m_Button_Back.setVisibility(View.VISIBLE);
         m_DownloadedFile_List = new ArrayList<>();
-
-        m_RecyclerView_DownloadedFile = findViewById(R.id.recyclerView_MyDownloads);
         m_DownloadFile_Adapter = new MyDownloadsAdapter(m_DownloadedFile_List);
-
-        RecyclerView.LayoutManager m_Layout_Manager = new LinearLayoutManager(getApplicationContext());
+        m_Layout_Manager = new LinearLayoutManager(getApplicationContext());
         m_RecyclerView_DownloadedFile.setLayoutManager(m_Layout_Manager);
         m_RecyclerView_DownloadedFile.setAdapter(m_DownloadFile_Adapter);
 
@@ -72,7 +83,7 @@ public class MyDownloadsActivity extends AppCompatActivity {
             public void onClick(View view, int position) {
 
                 String selectedFileName = m_DownloadedFile_List.get(position).getM_DownloadedFileName();
-                viewPdf(m_DownloadedFile_List.get(position).getM_DownloadedFileName());
+                viewPdf(selectedFileName);
             }
 
             @Override
@@ -88,6 +99,57 @@ public class MyDownloadsActivity extends AppCompatActivity {
                 destinationDetailIntent.putExtra(getResources().getString(R.string.userid), m_User_Id);
                 destinationDetailIntent.putExtra("username", m_User_Name);
                 startActivity(destinationDetailIntent);
+            }
+        });
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDrawerLayout.openDrawer(navigationView);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                // set item as selected to persist highlight
+                menuItem.setChecked(true);
+
+                if(menuItem.getTitle().equals("Courses")){
+
+                    Intent destinationDetailIntent = new Intent(getApplicationContext(), CoursesActivity.class);
+                    destinationDetailIntent.putExtra(getResources().getString(R.string.userid), m_User_Id);
+                    destinationDetailIntent.putExtra("username", m_User_Name);
+                    startActivity(destinationDetailIntent);
+
+                }else if(menuItem.getTitle().equals("Articles")){
+                    startActivity(new Intent(getApplicationContext(), ArticlesActivity.class));
+                }else if(menuItem.getTitle().equals("My Downloads")){
+
+                    mDrawerLayout.closeDrawers();
+                }else if(menuItem.getTitle().equals("My Results")){
+
+                    Intent destinationDetailIntent = new Intent(getApplicationContext(), MyResultsActivity.class);
+                    destinationDetailIntent.putExtra(getResources().getString(R.string.userid), m_User_Id);
+                    destinationDetailIntent.putExtra("username", m_User_Name);
+                    startActivity(destinationDetailIntent);
+
+                }else if(menuItem.getTitle().equals("Update Profile")){
+
+                    Intent destinationDetailIntent = new Intent(getApplicationContext(), UpdateProfileActivity.class);
+                    destinationDetailIntent.putExtra(getResources().getString(R.string.userid), m_User_Id);
+                    destinationDetailIntent.putExtra("username", m_User_Name);
+                    startActivity(destinationDetailIntent);
+                }else if(menuItem.getTitle().equals("Logout")){
+
+                }
+
+                // close drawer when item is tapped
+                mDrawerLayout.closeDrawers();
+
+                return true;
             }
         });
     }
